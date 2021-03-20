ec58e30f4993e67d17f24c56f54921d77c279904
annkimm <zizicoo208@naver.com>
2021-03-20 16:26:27 +0900

    dev: Add hashtag page

---
+++ src/pages/Hashtag.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { gray7 } from '../styles/const';
+
+export const TitleBox = styled.div`
+    padding: 18px 16px;
+`;
+
+export const Title = styled.h2`
+    font-size: 14px;
+    color: ${gray7};
+    font-weight: 600;
+`;
