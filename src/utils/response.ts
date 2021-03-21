512ac0743266d5456f6c3c1ec57d5d7528bfa5d6
annkimm <zizicoo208@naver.com>
2021-03-21 17:47:08 +0900

    dev: Add createPost page

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -41,6 +41,18 @@ export function getPost(postId: number) {
   return instance.get(`/post/${postId}`);
 }
 
+export function postPost(fileList:Array<File>,
+  tags: RegExpMatchArray | null, mention: string | null, location: string | null) {
+  const data = new FormData();
+
+  data.append('photos', JSON.stringify(fileList));
+  data.append('tags', JSON.stringify(tags));
+  data.append('mention', JSON.stringify(mention));
+  data.append('location', JSON.stringify(location));
+
+  return instance.post('/post/create', data);
+}
+
 export function postPassword(password: string, rePassword: string) {
   const data = new FormData();
 
