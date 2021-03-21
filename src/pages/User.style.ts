593eeecb6738bbf9f89734c6922615ab593478a5
annkimm <zizicoo208@naver.com>
2021-03-21 16:16:54 +0900

    dev: Add user page

---
+++ src/pages/User.style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { gray10 } from 'styles/const';
+
+export const Wrapper = styled.div`
+    height: 100%;
+
+    &.bgColor {
+        background-color: ${gray10};
+    }
+`;
