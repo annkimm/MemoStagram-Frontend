141383e78e4423d2e300e7cd678235ab6f411cf1
annkimm <zizicoo208@naver.com>
2021-03-22 10:43:06 +0900

    dev: Add new icon

--- src/utils/fontawesome.ts
+++ src/utils/fontawesome.ts
@@ -1,13 +1,28 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faKeyboard, faPlusSquare } from '@fortawesome/free-regular-svg-icons';
 import {
-  faArrowLeft, faEllipsisH, faPlusCircle, faPlusSquare as faSolidPlusSquare, faUser as faSolidUser,
+  faUser, faKeyboard, faPlusSquare, faComment, faSquare, faCalendarCheck, faHospital,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faArrowLeft,
+  faEllipsisH,
+  faPlusCircle, faPlusSquare as faSolidPlusSquare, faUser as faSolidUser, faArrowCircleLeft,
+  faArrowCircleRight, faTh, faCalendarDay, faTimes, faChevronRight, faMapMarkerAlt, faPhoneAlt,
+  faMap, faMapPin, faThumbtack, faMapMarkedAlt, faShoppingCart, faChild, faSchool,
+  faChalkboardTeacher, faParking, faGasPump, faSubway, faMoneyCheckAlt, faCamera, faHandshake,
+  faCoffee, faUtensils, faHSquare, faCapsules, faCashRegister, faLandmark, faUniversity,
+  faSearchLocation,
 } from '@fortawesome/free-solid-svg-icons';
 
 function initFontAwesome() {
   library.add(
     faUser, faSolidUser, faKeyboard, faArrowLeft,
-    faEllipsisH, faPlusCircle, faPlusSquare, faSolidPlusSquare,
+    faEllipsisH, faPlusCircle, faPlusSquare, faSolidPlusSquare, faArrowCircleLeft,
+    faArrowCircleRight, faComment, faTh, faSquare, faCalendarCheck, faCalendarDay,
+    faTimes, faChevronRight, faMapMarkerAlt, faPhoneAlt, faMap, faMapPin, faThumbtack,
+    faMapMarkedAlt, faShoppingCart, faChild, faSchool, faChalkboardTeacher, faParking,
+    faGasPump, faSubway, faMoneyCheckAlt, faCamera, faHandshake, faHospital, faCoffee,
+    faUtensils, faHSquare, faCapsules, faCashRegister, faLandmark, faUniversity,
+    faSearchLocation,
   );
 }
 
